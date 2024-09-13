7190da5717b38ecbc35888912e23ddf536e442a2
MAHAKAAL-Digital <commando.hayhayvanshi@gmail.com>
2024-09-13 03:40:13 +0000

    first commit

---
+++ max-of-3-nos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i , num ;
+        printf("enter a no. to get its table :");
+            scanf("%d",&num);
+                for(i=1;i<11;i++){
+                        printf("%d × %d = %d \n",num,i,num*i);
+                            
+
+                           }           return 0;
+}
